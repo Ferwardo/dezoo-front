8b6265b8e7845c9418f9df0331ff4f244fec8d25
Ferwardo <r0741339@student.thomasmore.be>
2020-12-21 15:03:36 +0100

    added framework for adding an editing personnel

--- public/js/index.js
+++ public/js/index.js
@@ -1,10 +1,41 @@
 $(() => {
     getPersonnel();
+
+    //reset everything back to normal after the personnelModal is closed
+    let personnelModal = document.getElementById("personnelModal")
+    personnelModal.addEventListener('hidden.bs.modal', (event) => {
+        $("#personnelModal").find(".modal-title").html("Nieuw Personeelslid")
+        $("#personnelModal").find(".form-control")[0].value = "";
+        $("#personnelModal").find(".form-control")[1].value = "";
+        $("#personnelModal").find(".form-control")[2].value = "";
+        $("#personnelModal").find(".form-control")[3].value = "";
+        $("#personnelModal").find(".form-control")[4].value = "";
+        $("#personnelModal").find(".form-control")[5].value = "";
+        $("#personnelModal").find(".form-select").val("notChosen").prop("selected", true);
+    })
 })
 
 //use this base url
-let url = "http://localhost:8050"
+let url = "http://localhost:8050";
+//set this to true when editing
+let editing = false;
 
+let openModal = (modalName) => {
+    let modal = new bootstrap.Modal(document.getElementById(modalName));
+    modal.show();
+}
+
+//add new personnel member or edit an old one to the db
+let postOrPutPersonnel = () => {
+
+}
+
+//edit personnel member
+let editPersonnel = () => {
+    editing = true;
+    $("#personnelModal").find(".modal-title").html("Editeer Personeelslid")
+    openModal("personnelModal")
+}
 
 //get all personnel members
 let getPersonnel = () => {
@@ -30,14 +61,14 @@ let getPersonnel = () => {
                     <td>${personnelMember.personelCategory}</td>\
                     <td>
                         <div class="btn-group">
-                            <button onclick="editPersoneel" class="btn btn-success" data-toggle="tooltip" title="Bewerk ${personnelMember.firstName} ${personnelMember.lastName}"><i class="far fa-edit"></i></button>
-                            <button onclick="deletePersoneel('${personnelMember.personnelId}')" class="btn btn-danger" data-toggle="tooltip" title="Verwijder ${personnelMember.firstName} ${personnelMember.lastName}"><i class="fas fa-trash"></i></button>
+                            <button onclick="editPersonnel()" class="btn btn-success" data-toggle="tooltip" title="Bewerk ${personnelMember.firstName} ${personnelMember.lastName}"><i class="far fa-edit"></i></button>
+                            <button onclick="deletePersonnel('${personnelMember.personnelId}')" class="btn btn-danger" data-toggle="tooltip" title="Verwijder ${personnelMember.firstName} ${personnelMember.lastName}"><i class="fas fa-trash"></i></button>
                         </div>
                     </td>
                 </tr>`
                 });
             }
-            $("#personnelTable").children().eq(1).append(tableContent);
+            $("#personnelTable").children().eq(1).html(tableContent);
         },
         error: () => {
             alert("Er is iet fout gelopen, probeer later opnieuw.")
@@ -46,7 +77,7 @@ let getPersonnel = () => {
 }
 
 //delete a personnel member
-let deletePersoneel = (personnelId) => {
+let deletePersonnel = (personnelId) => {
     $.ajax({
         url: url + "/personeel/" + personnelId,
         method: "DELETE",
